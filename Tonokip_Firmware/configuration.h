4c264cc30525eb8575f3de7298a6242554d4cd97
Nirav Patel <nrp@eclecti.cc>
2011-03-27 17:36:22 -0700

    local changes for my printer

--- Tonokip_Firmware/configuration.h
+++ Tonokip_Firmware/configuration.h
@@ -4,7 +4,7 @@
 // NO RS485/EXTRUDER CONTROLLER SUPPORT
 // PLEASE VERIFY PIN ASSIGNMENTS FOR YOUR CONFIGURATION!!!!!!!
 #define MOTHERBOARD 3 // ATMEGA168 0, SANGUINO 1, MOTHERBOARD = 2, MEGA 3, ATMEGA328 4
-#define SDSUPPORT 1
+//#define SDSUPPORT 1
 
 // THERMOCOUPLE SUPPORT UNTESTED... USE WITH CAUTION!!!!
 const bool USE_THERMISTOR = true; //Set to false if using thermocouple
@@ -14,11 +14,14 @@ const bool USE_THERMISTOR = true; //Set to false if using thermocouple
 // new_axis_steps_per_mm = previous_axis_steps_per_mm * (test_distance_instructed/test_distance_traveled)
 // units are in millimeters or whatever length unit you prefer: inches,football-fields,parsecs etc
 
+#define MICROSTEP_BY 2.0
+
 //Calibration variables
-float x_steps_per_unit = 80.376;
-float y_steps_per_unit = 80.376;
-float z_steps_per_unit = 3200/1.25;
-float e_steps_per_unit = 16;
+float x_steps_per_unit = 80.0/MICROSTEP_BY;	//64 for 10 tooth 5mm pulleys
+float y_steps_per_unit = 80.0/MICROSTEP_BY;	
+float z_steps_per_unit = 6667.0/MICROSTEP_BY;	
+//float e_steps_per_unit = 36.7;	//17.6 for adrians 36.7 for MakerGear extruder 14-17 for wades(Varies based on hobbing) ****these are all calculated for 16x microstepping
+float e_steps_per_unit = 365.0; //((4.0*200.0*13.0)/(math.pi*12.0))  //*((0.35/1.75)**2)
 float max_feedrate = 18000;
 
 //float x_steps_per_unit = 10.047;
@@ -33,25 +36,25 @@ const bool Y_ENABLE_ON = 0;
 const bool Z_ENABLE_ON = 0;
 const bool E_ENABLE_ON = 0;
 
-//Disables axis when it's not being used.
+//Disables axis when it's not being used. Z is the only one recommended to disable.
 const bool DISABLE_X = false;
 const bool DISABLE_Y = false;
 const bool DISABLE_Z = true;
 const bool DISABLE_E = false;
 
 const bool INVERT_X_DIR = false;
-const bool INVERT_Y_DIR = false;
+const bool INVERT_Y_DIR = true;
 const bool INVERT_Z_DIR = true;
 const bool INVERT_E_DIR = false;
 
 //Endstop Settings
-#define ENDSTOPPULLUPS 1
-const bool ENDSTOPS_INVERTING = false;
+//#define ENDSTOPPULLUPS 1
+const bool ENDSTOPS_INVERTING = true;
 const bool min_software_endstops = false; //If true, axis won't move to coordinates less than zero.
 const bool max_software_endstops = true;  //If true, axis won't move to coordinates greater than the defined lengths below.
-const int X_MAX_LENGTH = 220;
-const int Y_MAX_LENGTH = 220;
-const int Z_MAX_LENGTH = 100;
+const int X_MAX_LENGTH = 200;
+const int Y_MAX_LENGTH = 200;
+const int Z_MAX_LENGTH = 120;
 
 #define BAUDRATE 115200
 
--- Tonokip_Firmware/pins.h
+++ Tonokip_Firmware/pins.h
@@ -204,25 +204,23 @@
 #define X_DIR_PIN          28
 #define X_ENABLE_PIN       24
 #define X_MIN_PIN           3
-#define X_MAX_PIN           -2 //2
+#define X_MAX_PIN           2
 
 #define Y_STEP_PIN         38
 #define Y_DIR_PIN          40
 #define Y_ENABLE_PIN       36
 #define Y_MIN_PIN          16
-#define Y_MAX_PIN          -1 //17
+#define Y_MAX_PIN          17
 
 #define Z_STEP_PIN         44
 #define Z_DIR_PIN          46
 #define Z_ENABLE_PIN       42
-#define Z_MIN_PIN          -1 //18
-#define Z_MAX_PIN          -1 //19
+#define Z_MIN_PIN          18
+#define Z_MAX_PIN          19
 
 #define E_STEP_PIN         32
 #define E_DIR_PIN          34
 #define E_ENABLE_PIN       30
-
-#define LED_PIN            13
 
 //#define FAN_PIN            11 // UNCOMMENT THIS LINE FOR V1.0
 #define FAN_PIN            9 // THIS LINE FOR V1.1
@@ -231,11 +229,13 @@
 #define KILL_PIN           -1
 
 //#define HEATER_0_PIN        12  // UNCOMMENT THIS LINE FOR V1.0
-#define HEATER_0_PIN       10 // THIS LINE FOR V1.1
-#define HEATER_1_PIN       8 // THIS LINE FOR V1.1
+#define HEATER_0_PIN       8 // THIS LINE FOR V1.1
+#define TEMP_0_PIN          2   
 
-#define TEMP_0_PIN          2   // MUST USE ANALOG INPUT NUMBERING NOT DIGITAL OUTPUT NUMBERING!!!!!!!!!
-#define TEMP_1_PIN          1   // MUST USE ANALOG INPUT NUMBERING NOT DIGITAL OUTPUT NUMBERING!!!!!!!!!
+#define TEMP_1_PIN		1
+#define HEATER_1_PIN	10
+
+#define LED_PIN -1
 
 
 
